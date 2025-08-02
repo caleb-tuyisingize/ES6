7c44677cd1d9ad5ad13ef21276b9841a3f3db89e
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-08-02 14:31:06 +0200

    Classes test on just new objects inhertance static keyword in class

--- testForNew.js
+++ testForNew.js
@@ -17,3 +17,24 @@ const product1 = new Prodducts("Car",2500);
 product1.displayProducts();
 console.log(`Total-Price: ${product1.calculatingTotal(salesTax)}`);
 
+// FOR STATIC KEYWORD
+ 
+class MathUtil{
+    static PI = 3.14159;
+    static getDiacmeter(radius){
+        return radius * 2;
+    }
+    static getCircumference(radius){
+     return 2 * this.PI * radius;
+    }
+    static getArea(radius){
+
+        return this.PI * radius * radius;
+    }
+}
+// const mathUtil1 =new MathUtil()
+ 
+console.log(MathUtil.PI)
+console.log(MathUtil.getDiacmeter(23));
+console.log(MathUtil.getCircumference(10));
+console.log(MathUtil.getArea(10));
