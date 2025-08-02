c3daf862a2eac81f43ba32170d5ab30ffecea914
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-08-02 15:26:48 +0200

    Classes test on just new objects inhertance static keyword in class exercises about symbols with udacity

--- testForNew.js
+++ testForNew.js
@@ -101,3 +101,10 @@ myVehicle.honkHorn(); // beep beep
 const myBike = new Bicycle();
 myBike.honkHorn(); // honk honk
 
+const bowl = {
+  [Symbol('apple')]: { color: 'red', weight: 136.078 },
+  [Symbol('banana')]: { color: 'yellow', weight: 183.15 },
+  [Symbol('orange')]: { color: 'orange', weight: 170.097 },
+  [Symbol('banana')]: { color: 'yellow', weight: 176.845 }
+};
+console.log(bowl);
