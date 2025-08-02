b8130f19d4c261a2cf02a9b9f1a3ef2df43c236c
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-08-02 14:44:29 +0200

    Classes test on just new objects inhertance static keyword in class another example

--- testForNew.js
+++ testForNew.js
@@ -37,4 +37,26 @@ class MathUtil{
 console.log(MathUtil.PI)
 console.log(MathUtil.getDiacmeter(23));
 console.log(MathUtil.getCircumference(10));
-console.log(MathUtil.getArea(10));
+console.log(MathUtil.getArea(10)); 
+
+//  FOR CLASS USER EXAMPLE
+class User{
+
+    static userCount = 0;
+
+    constructor(username){ // is a special method used inside a JavaScript class to initialize (set up) a new object when it's created with the new keyword
+        this.username = username;
+        User.userCount++;
+    }
+
+
+}
+const user1 = new User("Mevis");
+console.log(user1.username);
+const user2 = new User("Elie");
+console.log(user2.username);
+const user3 = new User("Ishimwe");
+console.log(user3.username);
+// console.log(user1.username);
+console.log(user1.userCount); //UNDEFINED CAUSE USERCOUNT BELONGS TO THE CLASS USER CAUSE IT'S STATIC
+console.log(User.userCount); //INSTEAD USE CLASS NOT NEW OBJECT
