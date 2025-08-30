bc21b4bf6e592aafddfec2fe748339f08ea12d32
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-08-30 20:40:58 +0200

    The texts for falling like rain

---
+++ testing.js
@@ -0,0 +1,15 @@
+const string = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero magni dolorem assumenda placeat aliquid illo possimus dignissimos a, ratione nulla veniam esse, voluptatem iure recusandae reprehenderit culpa! Iusto, nobis necessitatibus?";
+
+const spelled = string.split("");
+const size = spelled.length;
+const newStr = [];
+spelled.forEach((index) => {
+    
+})
+for(let str = 0; str < size; str++){
+setTimeout((index)=> {
+ newStr.push(spelled[str]);
+},1000);
+
+}
+console.log(newStr)
