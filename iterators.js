ecd4bc3f792de4088eda7403a4c941b258bb008d
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-09-02 08:04:16 +0200

    The test on the javaScript next() and Symbol.iterator methods but to reach undefined

--- iterators.js
+++ iterators.js
@@ -4,3 +4,12 @@ const iterator = arr[Symbol.iterator]();
 console.log(iterator.next())
 
 console.log(iterator.next())
+console.log(iterator.next())
+
+console.log(iterator.next())
+
+console.log(iterator.next())
+
+console.log(iterator.next())
+
+console.log(iterator.next())
