e858146edcd8d494de9f8109b0d3ea5641f41e64
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-09-02 08:13:30 +0200

    The test on the javaScript looping over the generator function by testing it's characteristics

--- iterators.js
+++ iterators.js
@@ -13,3 +13,15 @@ console.log(iterator.next().value)
 console.log(iterator.next().value)
 
 console.log(iterator.next().value)
+
+function *numbers(){
+    yield 12;
+    yield 4;
+    yield 2;
+    yield 19;
+}
+
+const nums = numbers();
+
+console.log(typeof nums[Symbol.iterator])
+console.log(nums[Symbol.iterator]() === nums)
