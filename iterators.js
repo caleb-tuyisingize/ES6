43eeaec209a07fa524a9ffb94adc20e972d95d92
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-09-02 08:04:55 +0200

    The test on the javaScript next() and Symbol.iterator methods but to reach undefined now targeting only values

--- iterators.js
+++ iterators.js
@@ -1,15 +1,15 @@
 const arr = [3,2,55,3,45,5];
 const iterator = arr[Symbol.iterator]();
 
-console.log(iterator.next())
+console.log(iterator.next().value)
 
-console.log(iterator.next())
-console.log(iterator.next())
+console.log(iterator.next().value)
+console.log(iterator.next().value)
 
-console.log(iterator.next())
+console.log(iterator.next().value)
 
-console.log(iterator.next())
+console.log(iterator.next().value)
 
-console.log(iterator.next())
+console.log(iterator.next().value)
 
-console.log(iterator.next())
+console.log(iterator.next().value)
