813eb4d86b2f27a3355346f0df99d48cd8851828
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-09-03 08:17:23 +0200

    The test on proxy

--- proxiesTest.js
+++ proxiesTest.js
@@ -1,6 +1,10 @@
 const student = {};
 
 const proxyStudent = new Proxy(student, {
+    get(target, propert){
+       return `the Propert you are gettint is ${propert} 
+        and the Value is ${target[propert]}`;
+    },
   set(target, property, value) {
     if (property === "age" && typeof value !== "number") {
       throw new Error("Age must be a number");
@@ -10,5 +14,8 @@ const proxyStudent = new Proxy(student, {
   }
 });
 
-proxyStudent.age = 20;   // ✅ Works
-proxyStudent.age = "hi"; // ❌ Throws error
+proxyStudent.age = 20;
+proxyStudent.name = "Maria";
+console.log(proxyStudent.name)
+
+// proxyStudent.age = "hi"; // ❌ Throws error
