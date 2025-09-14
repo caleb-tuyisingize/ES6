91d360c97de5d6401ec9a73f977d8b9b067b0f5a
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-09-14 08:10:18 +0200

    The test for the thing

---
+++ goods.js
@@ -0,0 +1,12 @@
+class Many{
+    constructor(maning){
+        this.maning = maning;
+        return this.maning;
+        return this;
+    }
+}
+// const valueri = new Many("Man")
+Many.prototype.valueri = function(Someone){
+    
+}
+console.log(Many.valueri)
