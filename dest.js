059a55af456b8e0c4328fcaeca2131068d0e6724
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-09-24 18:11:03 +0200

    .

---
+++ dest.js
@@ -0,0 +1,21 @@
+/*
+function greet(user) {
+  console.log("Hi " + user.name + ", age " + user.age);
+}
+greet({name: "Alice", age: 20});
+*/
+
+function greet({name, age}) {
+  console.log(`Hi ${name}, age ${age}`);
+}
+greet({name: "Alice", age: 20});
+
+
+console.log(y)
+var y = 10;
+
+var money = 2000;
+money = 3000;
+console.log(money);
+var dessert = "Ice Creamjbyg";
+console.log(dessert);
