39d88ad1f668f35b21e1bfd10dfbc0076371cfea
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-10-07 11:27:52 +0200

    .

---
+++ blacketNotation.js
@@ -0,0 +1,9 @@
+const persons = {
+    name:"Kalisa",
+    age: 23,
+    level:1,
+}
+
+for(let person in persons){
+    console.log(persons[person]);
+}
---
+++ dotNotation.js
@@ -0,0 +1,9 @@
+const persons = {
+    name:"Kalisa",
+    age: 23,
+    level:1,
+}
+console.log(Object.values(persons))
+// for(let person of Object.values(persons)){
+//     console.log(person);
+// }
