71aac0dd4a4cc1081d2c2e804ef6caf643de85a9
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-10-12 20:33:31 +0200

    .

--- goods.js
+++ goods.js
@@ -11,5 +11,5 @@ class Many{
      console.log(`Hello My name is ${this.name} am  ${this.age} years old, I like  ${this.favPet} and also I like to drink  ${this.favDrink} am currently in ${this.occupation}`);
     }
 }
-let user1 = new greet("Caleb Tuyisingize", 20, "Dog", "Soft drink Splite", "Coder");
+let user1 = new greet("Caleb Tuyisingizehvuyg", 20, "Dog", "Soft drink Splite", "Coder");
 console.log(Many.greet);
